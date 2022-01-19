a3d1a179e2c9ce57ead3f6f6c5e69f9a93e114d9
Michael C. McCall <26819445+mccallc@users.noreply.github.com>
2022-01-19 13:42:42 -0500

    Update gui.py

--- dvcurator/gui.py
+++ dvcurator/gui.py
@@ -18,12 +18,22 @@ doi_entry = tk.Entry(root, textvariable=doi)
 config_label = tk.Label(root, text="Config file")
 config_entry = tk.Button(root, text="Select folder", command=find_config)
 
-config_label.grid(column=2, row=1)
-config_entry.grid(column=1, row=1)
-doi_label.grid(column=2, row=2)
-doi_entry.grid(column=1, row=2)
+config_entry.pack(side=tk.LEFT)
+config_label.pack(side=tk.LEFT)
+doi_entry.pack(side=tk.BOTTOM)
+doi_label.pack(side=tk.LEFT)
+#config_label.grid(column=2, row=1)
+#config_entry.grid(column=1, row=1)
+#doi_label.grid(column=2, row=2)
+#doi_entry.grid(column=1, row=2)
+
+issues = ["foo.md", "bar.md"]
+for issue in issues:
+    l = tk.Checkbutton(root, text=issue)
+    l.pack()
 
 quit = tk.Button(root, text="Exit", command=root.quit)
-quit.grid(column=2, row=3)
+quit.pack(side=tk.BOTTOM, fill="x")
+#quit.grid(column=2, row=3)
 
 root.mainloop()
