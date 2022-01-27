2192e177b9df394640d9678013fdc2706182af18
mccallc <26819445+mccallc@users.noreply.github.com>
2022-01-27 13:11:38 -0500

    relative package name referencing

--- dvcurator/gui.py
+++ dvcurator/gui.py
@@ -78,7 +78,7 @@ class MainApp(tk.Frame):
 			print("Error: No dataverse host specified")
 			return
 			
-		import dataverse
+		from . import dataverse
 		self.citation = dataverse.get_citation(self.host.get(), self.doi.get(), self.dv_token.get())
 		# citation['depositor'].split(', ')[0] is the last name of the depositor
 		self.folder_name = project_name(self.citation['depositor'].split(', ')[0], self.citation['title'])
@@ -91,7 +91,7 @@ class MainApp(tk.Frame):
 		self.pdfmetadata_button.config(state="normal")
 
 	def download_extract(self):
-		import dataverse
+		from . import dataverse
 		print("Downloading project, please wait...")
 		extracted_path = dataverse.download_dataset(self.host.get(), self.doi.get(), self.dv_token.get(), self.folder_name, self.dropbox.get())
 		print("Extracted to: " + extracted_path)
@@ -104,7 +104,7 @@ class MainApp(tk.Frame):
 			print("Error: no github repository specified")
 			return
 			
-		import github
+		from . import github
 		existing = github.search_existing(self.folder_name, self.repo.get(), self.gh_token.get())
 		if (existing):
 			print("Error: existing github issues!!")
@@ -123,7 +123,7 @@ class MainApp(tk.Frame):
 				print(issue.get() + " added to project")
 
 	def set_metadata(self):
-		import pdf_metadata
+		from . import pdf_metadata
 		import os.path 
 		if (os.path.isdir(self.dropbox.get())):
 			metadata_path = pdf_metadata.make_metadata_folder(self.dropbox.get(), self.folder_name)
