1aa5ea918c15a44b7691ac2a5f47a56289704368
mccallc <26819445+mccallc@users.noreply.github.com>
2022-12-16 22:47:15 +0500

    should use project-curation repo

--- dvcurator/hosts.py
+++ dvcurator/hosts.py
@@ -2,5 +2,5 @@ qdr_dataverse = "https://data.qdr.syr.edu"
 qdr_doi_path = qdr_dataverse + "/dataset.xhtml?persistentId="
 github_api='https://api.github.com'
 github_org = "QualitativeDataRepository"
-curation_repo = "QualitativeDataRepository/testing-demos"
+curation_repo = "QualitativeDataRepository/Project-Curation"
 pkg_repo = "QualitativeDataRepository/dvcurator-python"
