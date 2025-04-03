1bea1ad0f2f65a71e44695fc3db94ef99611ed85
mccallc <26819445+mccallc@users.noreply.github.com>
2025-04-03 16:43:26 -0400

    better install batch script

--- SU Lab Install.bat
+++ SU Lab Install.bat
@@ -5,12 +5,15 @@ mkdir Apps-SU
 
 set PROG=dvcurator
 set EXE=C:\Apps-SU\%PROG%_win.exe
-set LNK=H:\Desktop\%PROG%.url
+set LNK=%USERPROFILE%\Desktop\%PROG%.url
 
+echo Downloading %PROG%...
 for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/QualitativeDataRepository/dvcurator-python/releases/latest ^| find "browser_download_url" ^| findstr exe') do (
-    curl -kL %%B -o %EXE%
+    curl -kL %%B -o %EXE% >null 2&1
 )
+echo Download complete!
 
+echo Making desktop shortcut...
 echo [InternetShortcut] >> %LNK%
 echo URL=%EXE% >> %LNK%
 echo IconFile=%EXE% >> %LNK%
